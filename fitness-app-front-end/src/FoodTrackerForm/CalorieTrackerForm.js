f26d5168317059de18900115b0aa17a448000216
Colin <cmmatt14@gmail.com>
2022-09-01 22:21:12 -0400

    Added form logic and form submit handler

--- fitness-app-front-end/src/FoodTrackerForm/CalorieTrackerForm.js
+++ fitness-app-front-end/src/FoodTrackerForm/CalorieTrackerForm.js
@@ -1,20 +1,54 @@
 import classes from './CalorieTrackerForm.module.css';
+import { useState } from 'react';
 
 const CalorieTrackerForm = (props) => {
+
+    const [foodInput, setFoodInput] = useState('');
+    const [calorieInput, setCalorieInput] = useState('');
+
+    const foodInputIsValid = foodInput !== '';
+    const calorieInputIsValid = calorieInput !== '';
+
+    let formIsValid = false;
+    if(foodInputIsValid && calorieInputIsValid){
+        formIsValid = true;
+    };
+
+    const foodInputHandler = (event) => {
+        setFoodInput(event.target.value);
+    };
+
+    const calorieInputHandler = (event) => {
+        setCalorieInput(event.target.value);
+    };
+
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        console.log(foodInput, calorieInput);
+        setFoodInput('');
+        setCalorieInput('');
+    }
+
+
     return(
-        <form>
+        <form onSubmit={formSubmitHandler}>
             <div className={classes.trackerForm}>
             <label htmlFor='foodItem'>Enter Food Here</label>
             <input 
                 type='text'
+                value={foodInput}
+                onChange={foodInputHandler}
 
             />
             <label htmlFor='calories'>Enter Calories Here</label>
             <input 
                 type='text'
-
+                value={calorieInput}
+                onChange={calorieInputHandler}
             />
             </div>
+            <button disabled={!formIsValid}>Submit</button>
+            
 
         </form>
 
