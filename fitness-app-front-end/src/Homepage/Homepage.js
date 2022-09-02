7e783c339808751ffb407b1e5ec9849cf1494289
Colin <cmmatt14@gmail.com>
2022-09-01 21:47:55 -0400

    added homepage

---
+++ fitness-app-front-end/src/Homepage/Homepage.js
@@ -0,0 +1,14 @@
+import classes from './Homepage.module.css';
+
+const Homepage = (props) => {
+    return(
+      <div className={classes.homepage}>
+        <h1>
+            hello
+        </h1>
+      </div>  
+    );
+
+};
+
+export default Homepage;
