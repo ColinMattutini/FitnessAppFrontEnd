4f257527915078543cb217e81ea121a8e2e476f2
Colin <cmmatt14@gmail.com>
2022-09-09 16:02:04 -0400

    added hover over display for donut chart

--- fitness-app-front-end/src/components/Charts/DonutChart.js
+++ fitness-app-front-end/src/components/Charts/DonutChart.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import classes from './DonutChart.module.css';
-import { PieChart, Pie } from 'recharts'; 
+import { PieChart, Pie, Tooltip, Cell } from 'recharts'; 
 
 const DonutChart = () => {
+
+    const COLORS = ["#8884d8", "#82ca9d"];
 
     const data = [
         {name: 'Test', students: 40},
         {name: 'Test2', students: 80}
     ];
     
+    const CustomTooltip = ({active, payload, label}) => {
+        if(active) {
+            return(
+                <div className="custom-tooltip"
+                style={{
+                    backgroundColor: "#ffff",
+                    padding: "5px",
+                    border: "1px solid #cccc"
+                }}
+                >
+                    <label>{`${payload[0].name} : ${payload[0].value}`}</label>
+                </div>
+            );
+        }
+        return null;
+    };
 
     return(
         <div className={classes.donutChart}>
@@ -18,7 +36,18 @@ const DonutChart = () => {
             dataKey="students"
             outerRadius={200}
             innerRadius={120}
-            fill="blue" />
+            fill="blue">
+
+            {data.map((entry, index) => (
+                <Cell 
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+                />
+
+            ))}
+
+            </Pie>
+        <Tooltip content={<CustomTooltip />} />
         </PieChart>
         </div>
 
