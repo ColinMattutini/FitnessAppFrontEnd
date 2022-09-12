bdade92ee06b2edf991768e580c84f50a261ac3c
Colin <cmmatt14@gmail.com>
2022-09-12 16:22:42 -0400

    added logic for entering username and password and validate password

--- fitness-app-front-end/src/components/UserAuth/SignUpForm.js
+++ fitness-app-front-end/src/components/UserAuth/SignUpForm.js
@@ -1,26 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import LoginButton from '../UI/LoginButton';
 import Modal from '../UI/Modal';
 
 const SignUpForm = (props) => {
 
+    const [emailInput, setEmailInput] = useState('');
+    const [passwordInput, setPasswordInput] = useState('');
+    const [reenterPasswordInput, setReenterPasswordInput] = useState('');
+
+    let validPassword = (passwordInput === reenterPasswordInput);
+
+    const emailInputHandler = (event) => {
+        setEmailInput(event.target.value);
+    };
+
+    const passwordInputHandler = (event) => {
+        setPasswordInput(event.target.value);
+    };
+
+    const reenterPasswordInputHandler = event => {
+        setReenterPasswordInput(event.target.value);
+    };
+
+    const submitSignUpHandler = (event) => {
+        event.preventDefault();
+        if(validPassword){
+            console.log(emailInput, passwordInput);
+            props.hideSignUpForm();
+        } else{
+            alert('Passwords Do Not Match!');
+        };
+    };
+
     return(
         <Modal>
-        <form>
+        <form onSubmit={submitSignUpHandler}>
             <h1>Sign-Up With Email and Password</h1>
-            <label htmlFor='firstName'>First Name</label>
-            <input />
-            <label htmlFor='lastName'>Last Name</label>
-            <input />
             <label htmlFor='email'>E-mail</label>
-            <input />
+            <input 
+                onChange={emailInputHandler}
+            />
             <label htmlFor='password'>Password</label>
-            <input type='password' />
+            <input type='password' 
+                onChange={passwordInputHandler}
+            />
             <label htmlFor='passwordCheck'>Re-Enter Password</label>
-            <input type='password' />
+            <input type='password' 
+                onChange={reenterPasswordInputHandler}
+            />
             <LoginButton 
                 value={"close"} 
                 onClick={props.hideSignUpForm} />
+            <LoginButton 
+                value={"Sign-Up"}
+                
+            />
         </form>
         </Modal>
 
