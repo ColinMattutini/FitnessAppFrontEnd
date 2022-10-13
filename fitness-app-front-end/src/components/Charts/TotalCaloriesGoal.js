c490bc2924a80078cc613dafc9ba5046304d5ade
Colin <cmmatt14@gmail.com>
2022-10-12 22:07:48 -0400

    added put request to update total calorie goal

--- fitness-app-front-end/src/components/Charts/TotalCaloriesGoal.js
+++ fitness-app-front-end/src/components/Charts/TotalCaloriesGoal.js
@@ -1,19 +1,49 @@
 import React from 'react';
 import Modal from '../UI/Modal';
 import { useState } from 'react';
+import AuthContext from '../../context/user-auth';
+import { useContext } from 'react';
 
 const TotalCaloriesGoal = (props) => {
 
     const [dailyCalorieGoal, setCalorieGoal] = useState('');
 
+    const authCtx = useContext(AuthContext);
+
     const calorieGoalHandler = (event) => {
         setCalorieGoal(event.target.value);
     };
 
+    const fetchCalorieGoalHandler = async (dailyCalorieGoal) => {
+       try{
+            const response = await fetch(
+                "http://localhost:8080/api/caloriegoal/"+authCtx.UUID,
+                {
+                    method: 'PUT',
+                    body: JSON.stringify({
+                        username: authCtx.UUID,
+                        calorieGoal: dailyCalorieGoal
+                    }),
+                    headers: {
+                     'Content-Type':'application/json'
+                    },
+                }
+            );
+            authCtx.updatedStateHandler(110);
+            if(!response.ok){
+                throw new Error('Request Failed');
+            }
+
+        }catch (err){
+            
+        }
+    }
+
     const submitFormHandler = (event) => {
         event.preventDefault();
         setCalorieGoal(event.target.value);
-        props.calorieGoalHandler(dailyCalorieGoal);
+        //props.calorieGoalHandler(dailyCalorieGoal);
+        fetchCalorieGoalHandler(dailyCalorieGoal);
         props.hideCalorieGoalModalHandler();
     }
 
