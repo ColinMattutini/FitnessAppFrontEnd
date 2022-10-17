6cc6c8a8745f237b582d228631e5bf9e256e116c
Colin <cmmatt14@gmail.com>
2022-10-16 21:59:59 -0400

    navbar component added to caloriepage

--- fitness-app-front-end/src/pages/CalorieTrackerPage.js
+++ fitness-app-front-end/src/pages/CalorieTrackerPage.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import Header from "../components/Header/Header";
 import Homepage from "../components/Homepage/Homepage";
+import NavBar from "../components/NavBar/NavBar";
 
 const CalorieTrackerPage = () => {
     return (
     <Fragment>
         <Header />
+        <NavBar />
         <Homepage />
     </Fragment>
         );
