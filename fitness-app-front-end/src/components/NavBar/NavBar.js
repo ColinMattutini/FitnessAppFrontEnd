cc24d3928132df8a7f6fe9e4b33a3bf4ba29b3ed
Colin <cmmatt14@gmail.com>
2022-10-16 21:01:44 -0400

    added vertical nav bar to left side of screen

---
+++ fitness-app-front-end/src/components/NavBar/NavBar.js
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+import AuthContext from '../../context/user-auth';
+import classes from './NavBar.module.css';
+
+const NavBar = () => {
+
+    const authCtx = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    const workoutNavHandler = (event) => {
+        event.preventDefault();
+        authCtx.updatedStateHandler(200);
+        navigate('/workoutpage');
+    }
+
+    const calorieTrackerNavHandler = (event) => {
+        event.preventDefault();
+        authCtx.updatedStateHandler(250);
+        navigate('/homepage');
+    }
+    return (
+        <div className={classes.navbar}>
+            <div className={classes.holder}>
+            <button>Homepage</button>
+            <button onClick = {calorieTrackerNavHandler}>Calorie Tracker</button>
+            <button onClick = {workoutNavHandler}>Workouts</button>
+            <button>Holder</button>
+            <button>Settings</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default NavBar;
