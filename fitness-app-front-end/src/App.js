580a2e897994dfc43ac3827c980a5ac55a440136
Colin <cmmatt14@gmail.com>
2022-10-24 21:51:41 -0400

    redirect to homepage if user is already logged in

--- fitness-app-front-end/src/App.js
+++ fitness-app-front-end/src/App.js
@@ -5,59 +5,31 @@ import { useState } from 'react';
 import { useContext } from 'react';
 import AuthContext from './context/user-auth.js';
 import SignUpForm from './components/UserAuth/SignUpForm.js';
-import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Router, Routes } from 'react-router-dom';
 import AuthPage from './pages/AuthPage.js';
 import CalorieTrackerPage from './pages/CalorieTrackerPage.js';
 import SignUpPage from './pages/SignUpPage.js';
 import WelcomePage from './pages/WelcomePage.js';
 import WorkoutPage from './pages/WorkoutPage.js';
+import MainHomePage from './pages/MainHomePage.js';
+import GoalPage from './pages/GoalPage.js';
 
 function App() {
 
   const authCtx = useContext(AuthContext);
-  // const [showModal, setShowModal] = useState(false);
-  // const [showSignUpModal, setShowSignUpModal] = useState(false);
-
-  // const showModalHandler = () => {
-  //     setShowModal(true);
-  // };
-
-  // const hideModalHandler = () => {
-  //   setShowModal(false);
-  // };
-
-  // const showSignUpForm = () => {
-  //   setShowSignUpModal(true);
-  // };
-
-  // const hideSignUpFormHandler = () => {
-  //   setShowSignUpModal(false);
-  // }
-
 
   return (
     <div>
 
-        <Routes>
-          
+        <Routes>  
           {authCtx.isLoggedIn && <Route exact path = "/calorietrackerpage" element={<CalorieTrackerPage />} />}
-          <Route exact path ="/" element={<WelcomePage />} />
+          <Route exact path ="/" element={!authCtx.isLoggedIn ? <WelcomePage /> : <Navigate to="/homepage" />} />
           <Route exact path ="/authpage" element={<AuthPage/>} />
           <Route exact path ='/signuppage' element = {<SignUpPage />} />
           {authCtx.isLoggedIn && <Route exact path = '/workoutpage' element = {<WorkoutPage />} />}
+          {authCtx.isLoggedIn && <Route exact path = '/homepage' element = {<MainHomePage />} />}
+          {authCtx.isLoggedIn && <Route exact path = '/goalpage' element = {<GoalPage />} />}
         </Routes>
-      
-      
-      
-      {/* <Header 
-        showLoginForm={showModalHandler}
-        showSignUpForm={showSignUpForm}  
-      />   
-      {authCtx.isLoggedIn && <Homepage />}
-      
-      {showModal && <LoginForm hideLoginForm={hideModalHandler}/>}
-      {showSignUpModal && <SignUpForm hideSignUpForm={hideSignUpFormHandler}/>}
-      <DonutChart /> */}
     </div>
 
   );
